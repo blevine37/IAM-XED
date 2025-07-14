Reformatting intro printing and fixing static directory calculation
If static ensemble calculation is required, only first geometries from XYZ files will be read. I fixed this by modifying read_xyz() such that it reads just the amount of atoms it should not attempting to read more.

src/iamxed/io_utils.py
@@ -43,7 +43,8 @@ def read_xyz(filename: str) -> Tuple[List[str], np.ndarray]:
     with open(filename) as xyz:
         n_atoms = int(xyz.readline())
         _ = xyz.readline()
-        for line in xyz:
+        for _ in range(n_atoms):
+            line = xyz.readline()
             if len(line.strip()) == 0:
                 break
             try: